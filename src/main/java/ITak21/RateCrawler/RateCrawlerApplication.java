9e3824f8e24f41a57a29bced4ec400bd932336bc
Inchan Tak <ict970@gmail.com>
2024-05-12 16:40:01 +0900

    first commit

---
+++ src/main/java/ITak21/RateCrawler/RateCrawlerApplication.java
@@ -0,0 +1,13 @@
+package ITak21.RateCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ITak21/RateCrawler/RateCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ITak21.RateCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
