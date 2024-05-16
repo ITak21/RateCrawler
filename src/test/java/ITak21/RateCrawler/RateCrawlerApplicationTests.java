d0db3f65b0d397f7c0cf8279d256462b97aefa93
Inchan Tak <ict970@gmail.com>
2024-05-17 00:14:20 +0900

    insert 테스트

--- src/test/java/ITak21/RateCrawler/RateCrawlerApplicationTests.java
+++ src/test/java/ITak21/RateCrawler/RateCrawlerApplicationTests.java
@@ -1,11 +1,9 @@
 package ITak21.RateCrawler;
 
 import ITak21.RateCrawler.entity.FavoriteEntity;
-import ITak21.RateCrawler.entity.SearchEntity;
-import ITak21.RateCrawler.entity.UsersEntity;
+
 import ITak21.RateCrawler.repository.FavoriteRepository;
-import ITak21.RateCrawler.repository.SearchRepository;
-import ITak21.RateCrawler.repository.UsersRepository;
+
 import jakarta.transaction.Transactional;
 import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
@@ -23,11 +21,11 @@ import static org.springframework.test.util.AssertionErrors.assertNotNull;
 class RateCrawlerApplicationTests {
 
 	@Autowired
-//	private SearchRepository searchRepository;
+
 	private FavoriteRepository favoriteRepository;
 
 	@Test
-	/* favorite테이블 테스트 코드 */
+
 	public void testInsertFavorite() {
 		FavoriteEntity favorite = new FavoriteEntity();
 		favorite.setUserId("user123");
@@ -40,25 +38,6 @@ class RateCrawlerApplicationTests {
 		System.out.println("Saved favorite ID: " + savedFavorite.getFavId());
 
 	}
-
-	/* search테이블 테스트 코드 */
-//	public void testInsertSearch() {
-//		SearchEntity search = new SearchEntity();
-//		search.setUserId("user123");
-//		search.setSearchName("Test Search");
-//		search.setSearchCode("TS01");
-//		search.setSearchDate(LocalDateTime.now());
-//
-//		SearchEntity savedSearch = searchRepository.save(search);
-//
-//		assertNotNull( Integer.toString(savedSearch.getSearchId()), "Search ID should not be null after saving");
-//		System.out.println("Saved search ID: " + savedSearch.getSearchId());
-//
-//	}
-
-
-
-
 }
 
 
